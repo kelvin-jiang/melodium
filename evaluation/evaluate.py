f94528fda4204fca94803a1777a4bc65cc9fe59b
Kelvin Jiang <k.jiang1118@gmail.com>
2021-04-10 22:58:14 -0400

    fixed bugs around evaluation

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -15,51 +15,39 @@ def evaluate_melody(melody, melody_hop_size, reference_file, reference_hop_size,
     total_unvoiced = 0
     total = 0
 
-    melody_index = 0
-    with open(reference_file, 'r', encoding='utf-8') as f:
-        for i, line in enumerate(f):
-            _, ref_freq = line.strip().split()
-            ref_freq = float(ref_freq)
+    reference = load_melody(reference_file)
 
-            ref_time = i * (reference_hop_size / fs)
-            melody_start_time = melody_index * (melody_hop_size / fs)
-            melody_end_time = (melody_index + 1) * (melody_hop_size / fs)
-            while not melody_start_time <= ref_time <= melody_end_time:
-                melody_index += 1
-                melody_start_time = melody_index * (melody_hop_size / fs)
-                melody_end_time = (melody_index + 1) * (melody_hop_size / fs)
+    for i, melody_freq in enumerate(melody):
+        melody_time = i * (melody_hop_size / fs)
 
-            if melody_index == len(melody) - 1 or ref_time - melody_start_time <= melody_end_time - ref_time:
-                # closer to start frame in melody
-                index = melody_index
-            else:
-                # closer to end frame in melody
-                index = melody_index + 1
+        # get index of reference at the corresponding time of melody_time
+        ref_index = min(round(melody_time / (reference_hop_size / fs)), len(reference) - 1)
+        ref_freq = reference[ref_index]
 
-            # recall / false alarm
-            if melody[index] != 0 and ref_freq != 0:
-                recall += 1
-            elif melody[index] != 0 and ref_freq == 0:
-                false_alarm += 1
+        # recall / false alarm
+        if melody_freq != 0 and ref_freq != 0:
+            recall += 1
+        elif melody_freq != 0 and ref_freq == 0:
+            false_alarm += 1
 
-            # accuracy / pitch accuracy
-            if ref_freq * (2**(-0.5 / 12)) <= melody[index] <= ref_freq * (2**(0.5 / 12)):
-                pitch_accuracy += 1
-            if (melody[index] == 0 and ref_freq == 0) or (melody[index] != 0 and ref_freq != 0 and \
-                    ref_freq * (2**(-0.5 / 12)) <= melody[index] <= ref_freq * (2**(0.5 / 12))):
-                overall_accuracy += 1
+        # accuracy / pitch accuracy
+        if ref_freq * (2**(-0.5 / 12)) <= melody_freq <= ref_freq * (2**(0.5 / 12)):
+            pitch_accuracy += 1
+        if (melody_freq == 0 and ref_freq == 0) or (melody_freq != 0 and ref_freq != 0 and \
+                ref_freq * (2**(-0.5 / 12)) <= melody_freq <= ref_freq * (2**(0.5 / 12))):
+            overall_accuracy += 1
 
-            # total voiced / total unvoiced / total
-            if ref_freq != 0:
-                total_voiced += 1
-            else:
-                total_unvoiced += 1
-            total += 1
+        # total voiced / total unvoiced / total
+        if ref_freq != 0:
+            total_voiced += 1
+        else:
+            total_unvoiced += 1
+        total += 1
 
-    print(f'voiced recall rate: {recall / total_voiced}')
-    print(f'voiced false alarm rate: {false_alarm / total_unvoiced}')
-    print(f'raw pitch accuracy: {pitch_accuracy / total}')
-    print(f'overall accuracy: {overall_accuracy / total}')
+    print(f'voiced recall rate: {recall / total_voiced:.3f} ({recall} of {total_voiced})')
+    print(f'voiced false alarm rate: {false_alarm / total_unvoiced:.3f} ({false_alarm} of {total_unvoiced})')
+    print(f'raw pitch accuracy: {pitch_accuracy / total:.3f} ({pitch_accuracy} of {total})')
+    print(f'overall accuracy: {overall_accuracy / total:.3f} ({overall_accuracy} of {total})')
 
 def main():
     parser = argparse.ArgumentParser()
