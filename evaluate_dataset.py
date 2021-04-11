3a195c328e978628dd8db2b0d066597c4f639053
Kelvin Jiang <k.jiang1118@gmail.com>
2021-04-11 01:50:28 -0400

    evaluate dataset in hz instead of bins

--- evaluate_dataset.py
+++ evaluate_dataset.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from contour import create_contours, select_melody
 from evaluation import evaluate_melody
+from salience import get_hz_from_bin
 from spectral import hop_size
 from utils import load_melody
 
@@ -24,19 +25,20 @@ def main():
 
         saliences = np.load(salience_file)
         contours, space = create_contours(saliences, fs)
-        melody = select_melody(contours, space.shape[1], fs)
+        melody_bins = select_melody(contours, space.shape[1], fs)
+        melody_freqs = get_hz_from_bin(melody_bins)
 
         reference_file = f'{args.reference_dir}/{name}REF.txt'
-        melody_metrics = evaluate_melody(melody, hop_size, reference_file, args.reference_hop_size, 44100)
+        melody_metrics = evaluate_melody(melody_freqs, hop_size, reference_file, args.reference_hop_size, 44100)
         metrics.append(melody_metrics)
 
         print(f'done evaluating {salience_file}!')
 
     metrics_avg = np.mean(np.array(metrics), axis=0)
-    print(f'voiced recall rate: {metrics_avg[0]:.3f}')
-    print(f'voiced false alarm rate: {metrics_avg[1]:.3f}')
-    print(f'raw pitch accuracy: {metrics_avg[2]:.3f}')
-    print(f'overall accuracy: {metrics_avg[3]:.3f}')
+    print(f'voiced recall rate: {metrics_avg[0]: .3f}')
+    print(f'voiced false alarm rate: {metrics_avg[1]: .3f}')
+    print(f'raw pitch accuracy: {metrics_avg[2]: .3f}')
+    print(f'overall accuracy: {metrics_avg[3]: .3f}')
 
 if __name__ == '__main__':
     main()
